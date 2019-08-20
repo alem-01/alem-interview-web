f1c6a4f29f9af2cf3ecc99ee4cddd5906c668997
Tlekbai Ali <tlekbalkai@gmail.com>
2019-08-20 11:38:28 +0600

    add dockerfile, circleci.config.yml

---
+++ scripts/push.sh
@@ -0,0 +1,7 @@
+# @Author: Tlekbai Ali
+# @Date:   2019-07-18 13:37:40
+# @Last Modified by:   Tlekbai Ali
+# @Last Modified time: 2019-08-20 11:25:53
+
+app="alem-interview"
+docker push atlekbai/${app}:latest
---
+++ scripts/reload.sh
@@ -0,0 +1,7 @@
+# @Author: Tlekbai Ali
+# @Date:   2019-07-22 18:18:05
+# @Last Modified by:   Tlekbai Ali
+# @Last Modified time: 2019-08-20 11:34:11
+
+app="alem-interview"
+docker stop ${app} && sh scripts/start.sh
