ec6609f04b8139bd6a082cabf3687b173f5d0a3b
Adilyam <adliyamt@gmail.com>
2019-08-22 13:39:01 +0600

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
 		<footer className="page-footer font-small fixed-bottom">
 			<div className="footer-copyright text-center py-3">
 				Made with <span role="img" aria-labelledby="heart">❤️</span> by
-				<a href="https://github.com/Zulbukharov" target="_blank" rel="noopener noreferrer"> Zulbukharov </a>
-				and
+				<a href="https://github.com/Zulbukharov" target="_blank" rel="noopener noreferrer"> Zulbukharov</a>,
+				 <a href="https://github.com/Adilyam" target="_blank" rel="noopener noreferrer"> Adilyam</a> and
 				<a href="https://github.com/atlekbai" target="_blank" rel="noopener noreferrer"> Atlekbai</a>
 			</div>
 		</footer>
